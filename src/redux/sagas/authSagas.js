bbdbd300daa7adb6bd8068b0f52f19a3cc490bfc
Kirill <freshjs.ftd.@gmail.com>
2022-07-06 19:58:49 +0300

    Add auth saga

---
+++ src/redux/sagas/authSagas.js
@@ -0,0 +1,37 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import ACTION_TYPES from '../actions/types';
+import * as HTTP_API from '../../api/http';
+import AuthActionCreators from '../actions/authActionCreators';
+
+function * loginSaga (action) {
+  try {
+    const {
+      data: {
+        data: { user },
+      },
+    } = yield HTTP_API.login(action.payload);
+
+    yield put(AuthActionCreators.authSuccess(user));
+  } catch (error) {
+    yield put(AuthActionCreators.authError(error));
+  }
+}
+
+function * refreshSaga (action) {
+  try {
+    const {
+      data: {
+        data: { user },
+      },
+    } = yield HTTP_API.refresh(action.payload);
+
+    yield put(AuthActionCreators.authSuccess(user));
+  } catch (error) {
+    yield put(AuthActionCreators.authError(error));
+  }
+}
+
+export default function * authSagas () {
+  yield takeLatest(ACTION_TYPES.LOGIN_REQUEST, loginSaga);
+  yield takeLatest(ACTION_TYPES.REFRESH_REQUEST, refreshSaga);
+}
